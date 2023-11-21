297c804e6f958d5f23340d48ccfa9e95700351a1
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2023-11-21 15:37:19 +0900

    테스트입니다1

--- src/main/java/com/study/reactTest/HomeController.java
+++ src/main/java/com/study/reactTest/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController {
 		model.addAttribute("serverTime");
 		return "sendView";
 	}
-
+// 오창은입니다.
 	@RequestMapping(value = "/upload.do", method = RequestMethod.POST)
 	public void fileUpload(MultipartHttpServletRequest multipartRequest, HttpServletResponse response) {
 		// 추가데이터 테스트

32da8edfcd9589a1085384c4bd62825718ff93ed
OhChangEun <dwc07109@gmail.com>
2024-05-25 05:54:53 +0900

    문자열을 단어 단위로 쪼개는 함수

--- src/main/java/com/study/reactTest/StringtoArray.java
+++ src/main/java/com/study/reactTest/StringtoArray.java
@@ -1,12 +1,9 @@
 package com.study.reactTest;
 
 public class StringtoArray {
-    public static String[] stringToArray(){  // 긴 스트링을 스페이스 바 2칸 기준으로 나눠서 배열에 저장
-        //String food = "Gamjatang  Chicken Gomtang";
-        //String foodString = "불고기 김치 잡채 된장찌개 삼겹살 짱통";
-        String foodString =StaticData.FoodList;
-        // 현재는 임의로 감자탕이랑 닭곰탕으로 지정해놓음.
-        // 이부분은 나중에 전달받은 스트링을 함수변수로 받아오면 됨.
+    public static String[] stringToArray(String foodString){  // 긴 스트링을 스페이스 바 2칸 기준으로 나눠서 배열에 저장
+
+        // OCR한 결과로 나온 foodString을 단어 단위로 쪼개는 함수!!!
         String[] arr = foodString.split(" "); // 공백 2개
 
         for (String item : arr) {

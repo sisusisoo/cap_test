c4302d7e66100b4fee7c91f0c8c80a93fd399ab9
qwertyui6475 <qkrclals0307@naver.com>
2024-06-03 13:43:29 +0900

    1.ResInfo.js는 수정하기 전으로 롤백했습니다. 2.saveToFirebase.js -> 기존에는 checkIfDataExists 함수를 쓰기 위해 데이터 존재 여부를 확인하면서 한번, 데이터가 있을 때 getDataFromFirebase 함수를 써서 데이터를 가져오기 위해 호출하면서 총 2번의 DB 호출이 있었는데 이 두 함수를 하나로 합치면서 단일 DB 호출을 하게끔 변경했습니다. 3.MenuNullList.js는 기존의 ResInfo.js에서 음성과 리뷰기능만 제외하고 넘겨주었으며, 데이터가 안들어있을 때는 버튼이 나오게 하고, 데이터가 있으면 메뉴 이름을 순서대로 출력하게 해주었습니다.

--- src/main/ketchup/src/component/MenuNullList.js
+++ src/main/ketchup/src/component/MenuNullList.js
@@ -1,6 +1,8 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { MdRestaurantMenu } from "react-icons/md";
 import styled from "styled-components";
+import { useState, useEffect } from "react";
+import { saveDataToFirebase, updateMenuInFirebase, fetchDataFromFirebase } from "../component/saveToFirebase"; // Firebase 저장 및 업데이트 함수 임포트
 
 const Wrapper = styled.div`
   display: flex;
@@ -37,22 +39,76 @@ const Button = styled.button`
   padding: 2vh;
   width: 20vh;
 `;
+
+const MenuItem = styled.div`
+  font-size: 3vh;
+  margin: 1vh 0;
+`;
+
 const MenuNullList = () => {
-  const navigate = useNavigate();
+  const { place_id } = useParams();
+  const location = useLocation();
+  const [menu, setMenu] = useState(null);
+  const [menuItems, setMenuItems] = useState([]);
+  const [dataExists, setDataExists] = useState(false); // 데이터 존재 여부 상태 추가
+
+  const params = new URLSearchParams(location.search);
+  const name = params.get("name");
+  const placeLocation = params.get("location");
+
+  const key = `${name}-${placeLocation}`;
+
+  useEffect(() => {
+    const checkAndFetchData = async () => {
+      const data = await fetchDataFromFirebase(key);
+      if (data) {
+        setDataExists(true);
+        setMenu(data.menu);
+        if (data.menu) {
+          const menuArray = data.menu.split(",");
+          setMenuItems(menuArray);
+        }
+      } else {
+        setDataExists(false);
+        saveDataToFirebase(name, placeLocation, menu);
+      }
+    };
+
+    if (name && placeLocation) {
+      checkAndFetchData();
+    }
+  }, [name, placeLocation, key, menu]);
 
   const gotoPicture = () => {
-    navigate("/main/picture");
+    const newMenu = '감자탕,닭곰탕,칼국수';
+    updateMenuInFirebase(name, placeLocation, newMenu);
+    setMenu(newMenu);
+    setMenuItems(newMenu.split(","));
   };
 
   return (
     <Wrapper>
       <hr />
       <Icon />
-      <Text>메뉴 정보가 없습니다.</Text>
-      <Side>메뉴판 사진을 찍어주세요</Side>
-      <Button onClick={gotoPicture}>사진 찍기</Button>
+      {!dataExists ? (
+        <>
+          <Text>메뉴 정보가 없습니다.</Text>
+          <Side>메뉴판 사진을 찍어주세요</Side>
+          <Button onClick={gotoPicture}>사진 찍기</Button>
+        </>
+      ) : (
+        <>
+          <Text>메뉴 정보:</Text>
+          {menuItems.map((item, index) => (
+            <MenuItem key={index}>{index + 1}. {item}</MenuItem>
+          ))}
+        </>
+      )}
     </Wrapper>
   );
 };
 
 export default MenuNullList;
+
+
+
